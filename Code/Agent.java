40dfcaf7464ebac5814a7894354231e800fc0a75
CloudD233 <92987790+CloudD233@users.noreply.github.com>
2024-04-12 15:20:25 -0400

    Create Agent.java

---
+++ Code/Agent.java
@@ -0,0 +1,11 @@
+public class Agent {
+    Node start;
+    Node goal;
+    int id;
+
+    public Agent(int id, Node start, Node goal) {
+        this.id = id;
+        this.start = start;
+        this.goal = goal;
+    }
+}
